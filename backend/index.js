c451415268738d43e7d853484a8905e3c945e0b4
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2023-05-18 15:48:44 -0300

    Feat: ending init backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,17 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+  try{
+    const r = await axios.put(
+        'https://api.chatengine.io/users/',
+        {username: username, secret: username, first_name: username},
+        {headers: {"private-key": "b7778b35-1a77-4d9b-ba31-f67d5caa4da9"}}
+    )
+    return res.status(r.status).json(r.data);
+  } catch(e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
 app.listen(3001);
