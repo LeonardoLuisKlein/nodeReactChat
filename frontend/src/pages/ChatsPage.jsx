072b9842df02ccba31f2aa2c13e699ff3867f143
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2023-05-18 16:21:39 -0300

    Feat: adding the chats page

--- frontend/src/pages/ChatsPage.jsx
+++ frontend/src/pages/ChatsPage.jsx
@@ -1,5 +1,13 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic('b7778b35-1a77-4d9b-ba31-f67d5caa4da9',
+     props.user.username, 
+     props.user.secret);
+    return <div style={{ height: '100vh' }}>
+        <MultiChatSocket {...chatProps }/>
+        <MultiChatWindow {...chatProps} style={{ height: '100%'}}/>
+    </div>
 }
 
 export default ChatsPage
